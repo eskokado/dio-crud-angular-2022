bce8dbbfa42d18fe5edf8ae930f4b5b46e9e88cc
eskokado <eskokado@gmail.com>
2022-06-06 17:52:03 -0300

    feat: calls getUsers in components with interface and service user

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  apiUrl = 'https://sheet.best/api/sheets/10476056-02b2-43f1-9d5a-3a2046df3a16';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  constructor(private httpClient: HttpClient) { }
+
+  getUsers():Observable<User[]> {
+    return this.httpClient.get<User[]>(this.apiUrl);
+  }
+}
